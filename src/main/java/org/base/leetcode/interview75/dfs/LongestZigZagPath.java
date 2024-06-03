a98236e7d4839afcdbc52cc0aff68ca9587c7e30
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-06-03 13:45:46 -0700

    add zig zag stats

--- src/main/java/org/base/leetcode/interview75/dfs/LongestZigZagPath.java
+++ src/main/java/org/base/leetcode/interview75/dfs/LongestZigZagPath.java
@@ -1,16 +1,13 @@
 package org.base.leetcode.interview75.dfs;
 
-import java.util.function.Function;
-
 /**
  * <a href="https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75">
  *     1372. Longest ZigZag Path in a Binary Tree</a>
  * <p>
  *     Stats:
- *     Runtime:    0ms (100.00%)
- *     Memory: 42.33mb  (67.70%)
+ *     Runtime:    4ms (96.66%)
+ *     Memory: 55.36mb (27.29%)
  * </p>
- *
  */
 public class LongestZigZagPath {
 
