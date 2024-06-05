de41d2ccc479ccb48c17e108c3d416dbaec85b8c
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-06-05 13:12:44 -0700

    parse nested int list + refactoring

--- src/main/java/org/base/leetcode/util/Helper.java
+++ src/main/java/org/base/leetcode/util/Helper.java
@@ -1,13 +1,61 @@
 package org.base.leetcode.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public class Helper {
+    public static String[] toArray(String str) {
+        str = str.substring(1, str.length() - 1);
+        return str.split(",\\s?");
+    }
+
+    public static String[][] toNestedArray(String str) {
+        str = str.substring(2, str.length() - 2);
+        String[] arr = str.split("],\\[", -1);
+        String[][] matrix = new String[arr.length][];
+        for (int i = 0; i < arr.length; i++) {
+            matrix[i] = (arr[i].isBlank()) ? new String[0] : arr[i].split(",\\s?");
+        }
+
+        return matrix;
+    }
+
     public static int[] toIntArray(String str) {
-        str = str.replace("[", "").replace("]", "");
-        String[] arr = str.split(", ");
+        String[] arr = toArray(str);
         int[] result = new int[arr.length];
         for (int i = 0; i < result.length; i++)
             result[i] = Integer.parseInt(arr[i]);
 
         return result;
+    }
+
+    public static List<Integer> toIntList(String str) {
+        String[] arr = toArray(str);
+        List<Integer> list = new ArrayList<>();
+        for (String s : arr)
+            if (!s.isBlank())
+                list.add(Integer.parseInt(s));
+
+        return list;
+    }
+
+    public static List<List<Integer>> toNestedIntList(String str) {
+        String[][] arr = toNestedArray(str);
+        List<List<Integer>> list = new ArrayList<>();
+        for (String[] strings : arr) {
+            List<Integer> nest = new ArrayList<>();
+            for (String string : strings) nest.add(Integer.parseInt(string));
+            list.add(nest);
+        }
+        return list;
+    }
+
+    public static <T> List<T> toList(String str, Function<String, T> fxn) {
+        String[] arr = toArray(str);
+        List<T> list = new ArrayList<>();
+        for (String s : arr) list.add(fxn.apply(s));
+
+        return list;
     }
 }
--- src/test/java/org/base/leetcode/util/HelperTests.java
+++ src/test/java/org/base/leetcode/util/HelperTests.java
@@ -2,12 +2,56 @@ package org.base.leetcode.util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.base.leetcode.util.Helper.toIntArray;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.base.leetcode.util.Helper.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelperTests {
     @Test
     public void testToIntArray() {
-        assertArrayEquals(new int[] {1,2,3}, toIntArray("[1, 2, 3]"));
+        assertArrayEquals(new int[] {1,2,3}, toIntArray("[1,2,3]"));
+    }
+
+    @Test
+    public void testToNestedArray() {
+        String[][] expected1 = new String[][] { {"1"}, {"2"}, {"3"}, {} };
+        String[][] actual1 = toNestedArray("[[1],[2],[3],[]]");
+        assertNestedArrayEquals(expected1, actual1);
+
+        String[][] expected2 = new String[][] { {"1","3"}, {"3","0","1"}, {"2"}, {"0"} };
+        String[][] actual2 = toNestedArray("[[1,3],[3,0,1],[2],[0]]");
+        assertNestedArrayEquals(expected2, actual2);
+    }
+
+    @Test
+    public void testToIntList() {
+        assertEquals(List.of(1, 2, 3), toIntList("[1,2,3]"));
+        assertEquals(List.of(), toIntList("[]"));
+    }
+
+    @Test
+    public void testToList() {
+        assertEquals(List.of(1,2,3), toList("[1,2,3]", Integer::parseInt));
+        assertEquals(List.of("1","2","3"), toList("[1,2,3]", Function.identity()));
+    }
+
+    @Test
+    public void testToNestedIntList() {
+        assertEquals(List.of(List.of(1), List.of(2), List.of(3), List.of()),
+                toNestedIntList("[[1],[2],[3],[]]"));
+        assertEquals(List.of(List.of(1,3), List.of(3,0,1), List.of(2), List.of(0)),
+                toNestedIntList("[[1,3],[3,0,1],[2],[0]]"));
+    }
+
+    public static <T> void assertNestedArrayEquals(T[][] expected, T[][] actual) {
+        assertEquals(expected.length, actual.length);
+        for (int i = 0; i < actual.length; i++) {
+            assertEquals(expected[i].length, actual[i].length);
+            for (int j = 0; j < actual[i].length; j++)
+                assertEquals(expected[i][j], actual[i][j]);
+        }
     }
 }
