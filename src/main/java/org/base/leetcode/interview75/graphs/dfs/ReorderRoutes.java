47d906448b80891b821581e6e17dc1d25279e815
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-06-08 22:29:10 -0700

    fix description, link

--- src/main/java/org/base/leetcode/interview75/graphs/dfs/ReorderRoutes.java
+++ src/main/java/org/base/leetcode/interview75/graphs/dfs/ReorderRoutes.java
@@ -3,8 +3,8 @@ package org.base.leetcode.interview75.graphs.dfs;
 import java.util.*;
 
 /**
- * <a href="https://leetcode.com/problems/number-of-provinces/?envType=study-plan-v2&envId=leetcode-75">
- *     547. Number of Provinces</a>
+ * <a href="https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/?envType=study-plan-v2&envId=leetcode-75">
+ *     1466. Reorder Routes to Make All Paths Lead to the City Zero</a>
  * <p>
  *     Stats:
  *     Runtime:   34ms  (87.33%)
