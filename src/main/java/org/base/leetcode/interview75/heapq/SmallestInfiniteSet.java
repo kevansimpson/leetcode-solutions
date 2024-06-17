6da19532465c3a465c534631bc4b3afd418c328b
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-06-17 16:06:17 -0700

    optimized infinite set

--- src/main/java/org/base/leetcode/interview75/heapq/SmallestInfiniteSet.java
+++ src/main/java/org/base/leetcode/interview75/heapq/SmallestInfiniteSet.java
@@ -3,6 +3,7 @@ package org.base.leetcode.interview75.heapq;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * <a href="https://leetcode.com/problems/smallest-number-in-infinite-set/?envType=study-plan-v2&envId=leetcode-75">
@@ -10,29 +11,27 @@ import java.util.Set;
  * <p>
  *     Stats:
  *     Runtime:   9ms  (95.68%)
- *     Memory: 44.88mb (97.27%)
+ *     Memory: 44.78mb (98.72%)
  * </p>
  */
 public class SmallestInfiniteSet {
-    private final PriorityQueue<Integer> queue = new PriorityQueue<>();
-    private final Set<Integer> set = new HashSet<>();
+    private final TreeSet<Integer> set = new TreeSet<>();
     private int minimum = 1;
 
     public SmallestInfiniteSet() {
     }
 
     public int popSmallest() {
-        if (queue.isEmpty())
+        if (set.isEmpty())
             return minimum++;
 
-        int min = queue.poll();
+        int min = set.first();
         set.remove(min);
         return min;
     }
 
     public void addBack(int num) {
         if (minimum > num && !set.contains(num)) {
-            queue.offer(num);
             set.add(num);
         }
     }
