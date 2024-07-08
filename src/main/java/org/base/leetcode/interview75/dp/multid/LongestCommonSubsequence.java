9adf2c7f939cd8adeb138306dabc369fd7f14d4c
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-07-08 12:51:13 -0700

    avoid range checks in longest common subsequence

--- src/main/java/org/base/leetcode/interview75/dp/multid/LongestCommonSubsequence.java
+++ src/main/java/org/base/leetcode/interview75/dp/multid/LongestCommonSubsequence.java
@@ -5,31 +5,24 @@ package org.base.leetcode.interview75.dp.multid;
  *     1143. Longest Common Subsequence</a>
  * <p>
  *     Stats:
- *     Runtime:   22ms  (46.98%)
- *     Memory: 50.22mb  (87.24%)
+ *     Runtime:   19ms  (87.63%)
+ *     Memory: 50.34mb  (84.39%)
  * </p>
  */
 public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(), n = text2.length();
-        int[][] table = new int[m][n];
+        int[][] table = new int[m + 1][n + 1];
 
-        for (int row = 0; row < m; row++)
-            for (int col = 0; col < n; col++)
-                if (text1.charAt(row) == text2.charAt(col))
-                    table[row][col] = 1 + get(table, row - 1, col - 1);
+        for (int row = 1; row <= m; row++)
+            for (int col = 1; col <= n; col++)
+                if (text1.charAt(row - 1) == text2.charAt(col - 1))
+                    table[row][col] = 1 + table[row - 1][col - 1];
                 else
                     table[row][col] = Math.max(
-                            get(table, row, col - 1),
-                            get(table, row - 1, col));
+                            table[row][col - 1],
+                            table[row - 1][col]);
 
-        return table[m - 1][n - 1];
-    }
-
-    int get(int[][] table, int row, int col) {
-        if (row < 0 || col < 0 || row >= table.length || col >= table[row].length)
-            return 0;
-        else
-            return table[row][col];
+        return table[m][n];
     }
 }
